aaa229099a18ca8ad743a3cc5c4af066c9b4c983
Bagus Cahyono <bagusbudy@gmail.com>
2017-10-12 14:26:53 +0700

    release 2

--- server/controllers/articlesController.js
+++ server/controllers/articlesController.js
@@ -3,7 +3,47 @@ const Article = require('../models/Article')
 module.exports = {
   create: (req, res) => {
     Article.create({
-      
+      title: req.body.title,
+      content: req.body.content,
+      category: req.body.category,
+      author: req.body.author
     })
+    .then(article => {res.send(article)})
+    .catch(err => {res.send(err)})
+  },
+  getAll: (req, res) => {
+    Article.find({})
+    .then(articles => {res.send(articles)})
+    .catch(err => {re.send(err)})
+  },
+  getByUId: (req, res) => {
+    Article.find({author: req.params.userid})
+    .then(articles => {res.send(articles)})
+    .catch(err => {res.send(err)})
+  },
+  getByCId: (req, res) => {
+    Article.find({category: req.params.catid})
+    .then(articles => {res.send(articles)})
+    .catch(err => {res.send(err)})
+  },
+  getById: (req, res) => {
+    Article.find({_id: req.params.id})
+    .then(article => {res.send(article)})
+    .catch(err => {res.send(err)})
+  },
+  update: (req, res) => {
+    Article.findOneAndUpdate({_id: req.params.id},{
+      title: req.body.title,
+      content: req.body.content,
+      category: req.body.category,
+      author: req.body.author
+    })
+    .then(result => {res.send(result)})
+    .catch(err => {res.send(err)})
+  },
+  destroy: (req, res) => {
+    Article.remove({_id: req.params.id})
+    .then(result => {res.send(result)})
+    .catch(err => {res.send(err)})
   }
 }
